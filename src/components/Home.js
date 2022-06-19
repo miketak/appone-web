6157168c7eb54bce20e76b6c275546d73fec17d2
Michael Takrama <mtakrama@yahoo.com>
2022-06-18 22:06:07 -0500

    Ported application to clean base

---
+++ src/components/Home.js
@@ -0,0 +1,14 @@
+import React, { useState, useEffect } from "react";
+
+const Home = () => {
+
+  // To contain base functionality
+
+  return (
+    <div>
+      <h1>WELCOME TO THE TRAVEL AND TOUR ONE STOP SHOP</h1>
+    </div>
+  );
+};
+
+export default Home;
