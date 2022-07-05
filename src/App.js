942618b6e853cb011755bc67efa3d26bd473bd1a
Michael Takrama <mtakrama@yahoo.com>
2022-07-05 16:43:53 +0000

    wip

--- src/App.js
+++ src/App.js
@@ -1,71 +1,22 @@
-import { useState, useEffect } from "react";
-import { Routes, Route, Link } from "react-router-dom";
-import AuthService from "./services/auth.service";
-import Login from "./components/Login";
-import Signup from "./components/Signup";
-import Home from "./components/Home";
-import "bootstrap/dist/css/bootstrap.min.css";
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import { CssBaseline } from '@material-ui/core';
+import Header from './components/Header';
 
-function App() {
-  const [currentUser, setCurrentUser] = useState(undefined);
-
-  useEffect(() => {
-    const user = AuthService.getCurrentUser();
-
-    if (user) {
-      setCurrentUser(user);
-    }
-  }, []);
-
-  const logOut = () => {
-    AuthService.logout();
-  };
-
+const useStyles = makeStyles((theme) => ({
+  root: {
+    minHeight: '100vh',
+    backgroundImage: `url(${process.env.PUBLIC_URL + '/assets/bg.jpg'})`,
+    backgroundRepeat: 'no-repeat',
+    backgroundSize: 'cover',
+  },
+}));
+export default function App() {
+  const classes = useStyles();
   return (
-    <div>
-      <nav className="navbar navbar-expand navbar-dark bg-dark">
-        <div className="navbar-nav mr-auto">
-          <li className="nav-item">
-            <Link to={"/home"} className="nav-link">
-              Home
-            </Link>
-          </li>
-        </div>
-
-        {currentUser ? (
-          <div className="navbar-nav ms-auto">
-            <li className="nav-item">
-              <a href="/login" className="nav-link" onClick={logOut}>
-                Logout
-              </a>
-            </li>
-          </div>
-        ) : (
-          <div className="navbar-nav ms-auto">
-            <li className="nav-item">
-              <Link to={"/login"} className="nav-link">
-                Login
-              </Link>
-            </li>
-
-            <li className="nav-item">
-              <Link to={"/signup"} className="nav-link">
-                Sign up
-              </Link>
-            </li>
-          </div>
-        )}
-      </nav>
-
-      <div className="container mt-3">
-        <Routes>
-          <Route path="/home" element={<Home />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/signup" element={<Signup />} />
-        </Routes>
-      </div>
+    <div className={classes.root}>
+      <CssBaseline />
+      <Header />
     </div>
   );
 }
-
-export default App;
