f63747985bcafd09d6cbe3e0ef0aecbdb10d4076
Michael Takrama <mtakrama@yahoo.com>
2022-07-05 22:13:41 +0000

    wip

--- src/App.js
+++ src/App.js
@@ -1,22 +1,25 @@
-import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import { CssBaseline } from '@material-ui/core';
-import Header from './components/Header';
+import logo from './logo.svg';
+import './App.css';
 
-const useStyles = makeStyles((theme) => ({
-  root: {
-    minHeight: '100vh',
-    backgroundImage: `url(${process.env.PUBLIC_URL + '/assets/bg.jpg'})`,
-    backgroundRepeat: 'no-repeat',
-    backgroundSize: 'cover',
-  },
-}));
-export default function App() {
-  const classes = useStyles();
+function App() {
   return (
-    <div className={classes.root}>
-      <CssBaseline />
-      <Header />
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
+
+export default App;
