854b6d29c00f3fe47714c4987a6618062b11b27c
Michael Takrama <mtakrama@yahoo.com>
2022-07-07 03:22:50 +0000

    another major reset

--- src/App.js
+++ src/App.js
@@ -1,63 +1,23 @@
-import "./App.css";
-import travel_01 from "./assets/travel-01.jpg";
-import travel_02 from "./assets/travel-02.jpg";
-import travel_03 from "./assets/travel-03.jpg";
-import Hero from "./components/Hero";
-import Navbar from "./components/Navbar";
-import Slider from "./components/Slider";
-
-import AuthService from "./services/auth.service";
-// import { useState, useEffect } from "react";
-import { Routes, Route } from "react-router-dom";
-import Login from "./components/Login";
-import Signup from "./components/Signup";
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
-  // const [currentUser, setCurrentUser] = useState(undefined);
-
-  // useEffect(() => {
-  //   const user = AuthService.getCurrentUser();
-
-  //   if (user) {
-  //     setCurrentUser(user);
-  //   }
-  // }, []);
-
-  const logOut = () => {
-    AuthService.logout();
-  };
-
-  const navbarLinks = [
-    { url: "#", title: "Home" },
-    { url: "#", title: "Trips" },
-    { url: "#", title: "Rewards" },
-    { url: "/login", title: "Login" },
-    { url: "/signup", title: "Signup", onClick: {logOut} }
-  ];
-
   return (
     <div className="App">
-      <Navbar navbarLinks={navbarLinks} />
-      <Hero imageSrc={travel_01} />
-      <Slider
-        imageSrc={travel_02}
-        title={"Be an explorer."}
-        subtitle={
-          "Our platform offers a wide variety of unique travel locations!"
-        }
-      />
-      <Slider
-        imageSrc={travel_03}
-        title={"Memories for a lifetime."}
-        subtitle={"Your dream vacation is only a few clicks away."}
-        flipped={true}
-      />
-      {/* <div className="container mt-3">
-        <Routes>
-          <Route path="/login" element={<Login />} />
-          <Route path="/signup" element={<Signup />} />
-        </Routes>
-      </div> */}
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
