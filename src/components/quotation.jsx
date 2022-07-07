1aaf5f80eb09a3a5dc59cb62b29f8496286372d8
Michael Takrama <mtakrama@yahoo.com>
2022-07-07 05:08:25 +0000

    wip

--- src/components/quotation.jsx
+++ src/components/quotation.jsx
@@ -1,10 +1,28 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 class Quotation extends Component {
-    state = {  } 
-    render() { 
-        return <h2>Quotation Page</h2>
-    }
+  state = {};
+  render() {
+    return (
+      <React.Fragment>
+        <h2>Quotation Page</h2>
+        <ul>
+          <li>
+            <a href="#">Create Quotation</a>
+          </li>
+          <li>
+            <a href="#">Retrieve Quotations</a>
+          </li>
+          <li>
+            <a href="#">Update Quotation</a>
+          </li>
+          <li>
+            <a href="#">Delete Quotation</a>
+          </li>
+        </ul>
+      </React.Fragment>
+    );
+  }
 }
- 
+
 export default Quotation;
