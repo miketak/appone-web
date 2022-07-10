0dc54ba89bb52ea6cf1b666b9ce3d75824d5dc6d
Michael Takrama <mtakrama@yahoo.com>
2022-07-10 22:45:16 +0000

    progress

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
-import React from "react";
 import { Routes, Route, Navigate } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
+import React, { Component } from "react";
+import jwtDecode from "jwt-decode";
 import Navbar from "./components/navbar";
 import Home from "./components/home";
 import SignupForm from "./components/signupForm";
@@ -11,70 +12,63 @@ import ChangePassword from "./components/changePasswordForm";
 import Quotations from "./components/quotations";
 import QuotationForm from "./components/quotationForm";
 import LoginForm from "./components/loginForm";
+import logger from "./services/logService";
 import "react-toastify/dist/ReactToastify.css";
 import "./App.css";
 
-// async function componentDidMount() {
-//   // pending > resolved (success) OR rejected (failure)
-//   const { data: posts } = await axios.get(apiEndPoint);
-//   this.setState({ posts });
-// }
+class App extends Component {
+  state = {};
 
-// handleAdd = async () => {
-//   const obj = { title: "a", body: "b" };
-//   const { data: post } = await axios.post(apiEndPoint, obj);
-// };
+  componentDidMount() {
+    try {
+      const jwt = localStorage.getItem("token");
+      // const user = jwtDecode(jwt);
+      // console.log(user);
+      // this.setState({ user });
+      // TODO: Change this when jwt is implemented
+      //       on backend
+      if (jwt && jwt !== -1) {
+        logger.log(jwt);
+        // logger.log("jwt is not -1");
+        const user = { user: { name: "Fake Name" } };
+        // logger.log(user.name);
+        this.setState(user);
+      }
+    } catch (error) {}
+  }
 
-// handleUpdate = async () => {
-//   post.title = "UPDATED";
-//   const { data } = await axios.put(apiEndPoint + "/" + post.id, post);
-// };
-
-// handleDelete = async () => {
-//   const originalPosts = this.state.posts;
-
-//   const posts = this.state.posts.filter((p) => p.id !== post.id);
-//   this.setState({ posts });
-
-//   try {
-//     const { data } = await axios.delete(apiEndPoint + "/" + post.id);
-//   } catch (error) {
-//     alert("Something failed while deleting a post!");
-//     this.setState({ posts: originalPosts });
-//   }
-// };
-
-function App() {
-  return (
-    <React.Fragment>
-      <ToastContainer />
-      <Navbar />
-      <main className="container">
-        <Routes>
-          <Route path="/home" element={<Home />} />
-          <Route path="/login" element={<LoginForm />} />
-          <Route path="/signup" element={<SignupForm />} />
-          <Route path="/forgotpassword" element={<ForgotPassword />} />
-          <Route exact path="/not-found" element={<NotFound />} />
-          <Route path="/dashboard" element={<Dashboard />}>
-            <Route index element={<Quotations />} />
-            <Route path="/dashboard/quotations" element={<Quotations />} />
-            <Route
-              path="/dashboard/quotations/:id"
-              element={<QuotationForm />}
-            />
-            <Route
-              path="/dashboard/changepassword"
-              element={<ChangePassword />}
-            />
+  render() {
+    return (
+      <React.Fragment>
+        <ToastContainer />
+        <Navbar user={this.state.user} />
+        <main className="container">
+          <Routes>
+            <Route path="/home" element={<Home />} />
+            <Route path="/login" element={<LoginForm />} />
+            <Route path="/signup" element={<SignupForm />} />
+            <Route path="/forgotpassword" element={<ForgotPassword />} />
+            <Route exact path="/not-found" element={<NotFound />} />
+            <Route path="/dashboard" element={<Dashboard />}>
+              <Route index element={<Quotations />} />
+              <Route path="/dashboard/quotations" element={<Quotations />} />
+              <Route
+                path="/dashboard/quotations/:id"
+                element={<QuotationForm />}
+              />
+              <Route
+                path="/dashboard/changepassword"
+                element={<ChangePassword />}
+              />
+              <Route path="*" element={<Navigate to="/not-found" />} />
+            </Route>
+            <Route exact path="/" element={<Home />} />
             <Route path="*" element={<Navigate to="/not-found" />} />
-          </Route>
-          <Route exact path="/" element={<Home />} />
-          <Route path="*" element={<Navigate to="/not-found" />} />
-        </Routes>
-      </main>
-    </React.Fragment>
-  );
+          </Routes>
+        </main>
+      </React.Fragment>
+    );
+  }
 }
 
 export default App;
--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,41 +1,54 @@
 import React, { Component } from "react";
-import "bootstrap/dist/css/bootstrap.css";
 import { Link, NavLink } from "react-router-dom";
+import logger from "../services/logService";
+import "bootstrap/dist/css/bootstrap.css";
 
-class Navbar extends Component {
-  render() {
-    return (
-      <nav className="navbar navbar-expand-lg navbar-light bg-dark">
-        <Link className="navbar-brand" to="/">
-          CNAYA'S TRAVEL PORTAL
-        </Link>
-        <button
-          className="navbar-toggler"
-          type="button"
-          data-bs-toggle="collapse"
-          data-bs-target="#navbarNav"
-          aria-controls="navbarNav"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-          <span className="navbar-toggler-icon"></span>
-        </button>
-        <div className="collapse navbar-collapse" id="navBarAltMarkup">
-          <div className="navbar-nav">
-            <NavLink className="nav-item nav-link" to="/home">
-              Home
-            </NavLink>
-            <NavLink className="nav-item nav-link" to="/login">
-              Login
-            </NavLink>
-            <NavLink className="nav-item nav-link" to="/signup">
-              Sign Up
-            </NavLink>
-          </div>
+const Navbar = ({ user }) => {
+  return (
+    <nav className="navbar navbar-expand-lg navbar-light bg-dark">
+      <Link className="navbar-brand" to="/">
+        CNAYA'S TRAVEL PORTAL
+      </Link>
+      <button
+        className="navbar-toggler"
+        type="button"
+        data-bs-toggle="collapse"
+        data-bs-target="#navbarNav"
+        aria-controls="navbarNav"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span className="navbar-toggler-icon"></span>
+      </button>
+      <div className="collapse navbar-collapse" id="navBarAltMarkup">
+        <div className="navbar-nav">
+          <NavLink className="nav-item nav-link" to="/home">
+            Home
+          </NavLink>
+          {!user && (
+            <React.Fragment>
+              <NavLink className="nav-item nav-link" to="/login">
+                Login
+              </NavLink>
+              <NavLink className="nav-item nav-link" to="/signup">
+                Sign Up
+              </NavLink>
+            </React.Fragment>
+          )}
+          {user && (
+            <React.Fragment>
+              <NavLink className="nav-item nav-link" to="">
+                {user.name}
+              </NavLink>
+              <NavLink className="nav-item nav-link" to="">
+                Log Out
+              </NavLink>
+            </React.Fragment>
+          )}
         </div>
-      </nav>
-    );
-  }
-}
+      </div>
+    </nav>
+  );
+};
 
 export default Navbar;
